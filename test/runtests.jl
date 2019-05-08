432a6ee4d6f2243e4be949b3be0ef5e0237c78f2
tweisser <tillmann.weisser@web.de>
2019-05-08 14:09:20 -0600

    runtests

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+
+using .GMP
+using Test
+using DynamicPolynomials
+using SemialgebraicSets
+using SumOfSquares
+using CSDP
+
+
+include("meas.jl")
+include("momexpr.jl")
+include("momcon.jl")
+include("macros.jl")
