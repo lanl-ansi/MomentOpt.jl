159eee45acbe68d675b1dea54ade6c2c720367ab
tweisser <tillmann.weisser@web.de>
2020-02-28 11:18:57 -0700

    silent SCS

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ using OrderedCollections
 using LinearAlgebra
 
 using SCS
-factory =  with_optimizer(SCS.Optimizer, verbose = 1, eps = 1e-16)
+factory =  with_optimizer(SCS.Optimizer, verbose = 0, eps = 1e-16)
 
 include("meas.jl")
 include("momexpr.jl")
