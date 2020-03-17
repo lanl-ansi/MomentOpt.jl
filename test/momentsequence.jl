9c39d62ddbf5ec03ff32e7d54a2ca132ec97db5e
tweisser <tillmann.weisser@web.de>
2020-03-17 17:02:00 -0600

    fix sprint

--- test/momentsequence.jl
+++ test/momentsequence.jl
@@ -1,7 +1,7 @@
 @testset "moment sequence" begin
     @polyvar x y
     ms = moment_sequence(maxdegree_basis(ChebyshevBasis, [x], 1), ones(2))
-    @test sprint(show, ms) == "Dict(x=>1.0,1.0=>1.0)"
+    @test sprint(show, ms) == "Dict(x => 1.0,1.0 => 1.0)"
     @test_throws AssertionError moment_sequence(maxdegree_basis(ScaledMonomialBasis, [x], 2), ones(4))
 
     ls = lebesgue_box_sequence(variable_box(x => [-1,1], y => [-1,1]), 4; basis_type = LegendreBasis, normalize = true)
